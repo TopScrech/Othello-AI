568c2c6dfdb545a0c25a1b9bba2ed6e274fe8e91
TopScrech <sergei_saliukov@icloud.com>
2025-12-08 15:04:13 +0100

    Initial Commit

---
+++ Othello-SwiftUI/Othello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Othello_SwiftUIApp.swift
+//  Othello-SwiftUI
+//
+//  Created by Sergei Saliukov on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Othello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
